2d9455a86187cefaea8310cc963882f16062ce9b
Beta7x <adepriyantowidies@gmail.com>
2022-12-11 00:31:51 +0700

    Initial project

---
+++ src/layouts/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2 className="text-2xl text-black tracking-widest font-semibold title-font mb-1">
+        Whereas recognition of the inherent dignity
+      </h2>
+    </div>
+  );
+}
+
+export default Home;
