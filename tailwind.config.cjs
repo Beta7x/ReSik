4d9aec8c4c422d10bf9099f2a46174aee62a6219
Ihza Maulana Zakiya <sidoelz.123@gmail.com>
2022-12-12 06:28:41 +0700

    add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      colors:{
-        primary:"#47A359",
-        secondary: "#86CA94",
-        tertiary:"#EFF8F1",
-        text:{
-          primary:"#2F2F2F",
-          secondary:"#959595"
-        }
+      colors: {
+        primary: '#47A359',
+        secondary: '#86CA94',
+        tertiary: '#EFF8F1',
+        text: {
+          primary: '#2F2F2F',
+          secondary: '#959595',
+        },
       },
     },
   },
